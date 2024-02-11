0ac6fb4233bd1296b0cb7049428f4d2cc509b720
Luiz Lima <lippoko@LippoKo.local>
2024-02-11 22:18:25 +0000

    challenge03

--- challenges/challenge03.js
+++ challenges/challenge03.js
@@ -18,3 +18,38 @@ function calculateFactorial(number) {
     }
 }
 console.log(calculateFactorial(5));
+
+function dolarConvert(dolars) {
+    let cotaçao = 4.8;
+    let convert = dolars * cotaçao;
+    return convert;
+}
+console.log(dolarConvert(5));
+
+function calcularPerimetro(altura, largura) {
+    let area = altura * largura;
+    let perimetro = 2 * (altura + largura);
+    console.log(`Area: ${area}, Perimetro: ${perimetro}`);
+}
+calcularPerimetro(4, 3);
+
+function calculateCircle(radius) {
+    const PI = 3.14;
+
+    let area = PI * radius * radius;
+
+    let perimeter = 2 * PI * radius;
+
+    console.log(`Area: ${area}, Perimeter: ${perimeter}`);
+}
+
+calculateCircle(8);
+
+function displayMultiplicationTable(number) {
+    for (let i = 1; i <= 10; i++) {
+        let result = number * i;
+        console.log(number + 'x' + i + ' = ' + result);
+    }
+}
+
+displayMultiplicationTable(9);
